fe53bb7748dffcca43bfb29af3d6e33ff0b82054
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2023-12-20 16:00:23 -0300

    Add FavAddNew component to CalendarPage and update styles.css

--- src/calendar/index.js
+++ src/calendar/index.js
@@ -2,6 +2,7 @@
 
 export * from './components/CalendarEvent';
 export * from './components/CalendarModal';
+export * from './components/FavAddNew';
 export * from './components/Navbar';
 
 export * from './pages/CalendarPage';
--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -2,8 +2,7 @@ import { useState } from 'react';
 import { Calendar } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
-import { addHours } from 'date-fns';
-import { CalendarEvent, CalendarModal, Navbar } from "../";
+import { CalendarEvent, CalendarModal, FavAddNew, Navbar } from "../";
 
 import { localizer, getMessagesES } from '../../helpers';
 import { useUiStore, useCalendarStore } from '../../hooks';
@@ -67,6 +66,7 @@ export const CalendarPage = () => {
       />
 
       <CalendarModal />
+      <FavAddNew />
 
     </>
   )
