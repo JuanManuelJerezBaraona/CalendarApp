571059a2bbe433ff90b4562d9987818db559740f
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2023-12-28 10:46:40 -0300

    Add onUpdateEvent action to useCalendarStore.js and calendarSlice.js

--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
-import { onAddNewEvent, onSetActiveEvent } from "../store";
+import { onAddNewEvent, onSetActiveEvent, onUpdateEvent } from "../store";
 
 
 export const useCalendarStore = () => {
@@ -17,9 +17,10 @@ export const useCalendarStore = () => {
         // Todo bien
         if(calendarEvent._id) {
             // Actualizando
+            dispatch(onUpdateEvent({...calendarEvent}));
         } else {
             // Creando
-            dispatch(onAddNewEvent({...calendarEvent, _id: new Date().getTime()}))
+            dispatch(onAddNewEvent({...calendarEvent, _id: new Date().getTime()}));
         }
     }
     
--- src/store/calendar/calendarSlice.js
+++ src/store/calendar/calendarSlice.js
@@ -30,8 +30,17 @@ export const calendarSlice = createSlice({
         onAddNewEvent: (state, {payload}) => {
             state.events.push(payload);
             state.activeEvent = null;
+        },
+        onUpdateEvent: (state, {payload}) => {
+            state.events = state.events.map(event => {
+                if(event._id === payload._id) {
+                    return payload;
+                }
+
+                return event;
+            });
         }
     }
 });
 
-export const { onSetActiveEvent, onAddNewEvent } = calendarSlice.actions;
+export const { onSetActiveEvent, onAddNewEvent, onUpdateEvent } = calendarSlice.actions;
