a528c8525de7ac5ffab437e3f0aaa002f9d26a62
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2024-01-02 11:06:34 -0300

    Refactor imports and add useAuthStore hook

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,4 +1,4 @@
-import { useForm } from '../../hooks';
+import { useAuthStore, useForm } from '../../hooks';
 import './LoginPage.css';
 
 const loginFormFields = {
@@ -15,12 +15,14 @@ const registerFormFields = {
 
 export const LoginPage = () => {
 
+    const { startLogin } = useAuthStore();
+
     const { loginEmail, loginPassword, onInputChange: onLoginInputChange } = useForm(loginFormFields);
     const { registerName, registerEmail, registerPassword, registerPassword2, onInputChange: onRegisterInputChange } = useForm(registerFormFields);
 
     const loginSubmit = (e) => {
         e.preventDefault();
-        console.log(loginEmail, loginPassword);
+        startLogin({ email: loginEmail, password: loginPassword });
     }
 
     const registerSubmit = (e) => {
