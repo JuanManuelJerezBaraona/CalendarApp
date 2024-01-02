cf97d9ea167438fd559de9abe52e30d38ec9eeda
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2024-01-02 10:20:29 -0300

    Update authStatus value in AppRouter.jsx

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -5,7 +5,7 @@ import { CalendarPage } from "../calendar";
 
 export const AppRouter = () => {
 
-    const authStatus = 'authenticated';
+    const authStatus = 'not-authenticated';
 
   return (
     <Routes>
