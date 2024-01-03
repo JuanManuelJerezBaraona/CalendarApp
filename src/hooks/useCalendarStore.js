ec2e9e9feb341e00dc76778807b72599c1a86446
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2024-01-03 08:11:32 -0300

    Update useCalendarStore hook to save events to backend

--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -1,26 +1,31 @@
 import { useDispatch, useSelector } from "react-redux";
 import { onAddNewEvent, onDeleteEvent, onSetActiveEvent, onUpdateEvent } from "../store";
+import { calendarApi } from "../api";
 
 
 export const useCalendarStore = () => {
 
     const dispatch = useDispatch();
     const {events, activeEvent} = useSelector(state => state.calendar);
+    const {user} = useSelector(state => state.auth);
 
     const setActiveEvent = (calendarEvent) => {
         dispatch(onSetActiveEvent(calendarEvent));
     }
 
     const startSavingEvent = async(calendarEvent) => {
-        // TODO: llegar al backend
+        // TODO: Update event
 
-        // Todo bien
+    
         if(calendarEvent._id) {
             // Actualizando
             dispatch(onUpdateEvent({...calendarEvent}));
         } else {
             // Creando
-            dispatch(onAddNewEvent({...calendarEvent, _id: new Date().getTime()}));
+            const { data } = await calendarApi.post('/events', calendarEvent);
+            console.log(data);
+
+            dispatch(onAddNewEvent({...calendarEvent, id: data.event.id, user}));
         }
     }
 
