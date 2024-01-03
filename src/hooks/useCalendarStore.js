a90c31b5f705e704a3da6f5dc4109c0012b97bce
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2024-01-03 09:00:22 -0300

    Add async/await to startDeletingEvent function

--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -35,9 +35,15 @@ export const useCalendarStore = () => {
         }
     }
 
-    const startDeletingEvent = () => {
+    const startDeletingEvent = async() => {
         // Todo: llegar al backend
-        dispatch(onDeleteEvent());
+        try {
+            await calendarApi.delete(`/events/${ activeEvent.id }`);
+            dispatch(onDeleteEvent());
+        } catch (error) {
+            console.log(error);
+            Swal.fire('Error al eliminar el evento', error.response.data.msg, 'error');
+        }
     }
 
     const startLoadingEvents = async() => {
