8a16663d81a7cee6aaddb11056fcc4a4243ea28b
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2024-01-03 08:53:41 -0300

    Add useAuthStore hook to CalendarPage.jsx

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -5,10 +5,11 @@ import 'react-big-calendar/lib/css/react-big-calendar.css';
 import { CalendarEvent, CalendarModal, FavAddNew, FavDelete, Navbar } from "../";
 
 import { localizer, getMessagesES } from '../../helpers';
-import { useUiStore, useCalendarStore } from '../../hooks';
+import { useUiStore, useCalendarStore, useAuthStore } from '../../hooks';
 
 export const CalendarPage = () => {
 
+  const {user} = useAuthStore();
   const {openDateModal} = useUiStore();
   const {events, setActiveEvent, startLoadingEvents} = useCalendarStore();
   
@@ -16,8 +17,10 @@ export const CalendarPage = () => {
 
   const eventStyleGetter = ( event, start, end, isSelected ) => {
 
+    const isMyEvent = ( user.uid === event.user._id ) || ( user.uid === event.user._uid );
+
     const style = {
-      backgroundColor: '#367CF7',
+      backgroundColor: isMyEvent ? '#367CF7' : '#465660',
       borderRadius: '0px',
       opacity: 0.8,
       color: 'white'
